0db70cb1188ecb60c1128f2c053164486fe083ff
Roman Lugovtsev <omelemon@gmail.com>
2017-04-07 12:12:08 +0400

    Added codeclimate test reporter gems.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,10 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'simplecov'
 require 'collectible'
 require 'csv'
 # require 'pry'
+
+SimpleCov.start
 
 def load_collection(filename)
   file = File.read(File.expand_path("./fixtures/#{ filename }.csv", File.dirname(__FILE__)))
