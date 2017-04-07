3d647385bf55430a36352fa3424b37c0380879dd
Roman Lugovtsev <omelemon@gmail.com>
2017-04-07 11:57:15 +0400

    Commented pry for proper CI testing.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'collectible'
 require 'csv'
-require 'pry'
+# require 'pry'
 
 def load_collection(filename)
   file = File.read(File.expand_path("./fixtures/#{ filename }.csv", File.dirname(__FILE__)))
